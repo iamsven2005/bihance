7b15ae76ab4f2fcb1c3c48cc32cf2d36fb8ce962
Tan Sven <iamsven2005@gmail.com>
2024-08-11 00:53:55 +0800

    oh no anyways, workspace notfound

--- app/workspace/[[...workspace]]/page.tsx
+++ app/workspace/[[...workspace]]/page.tsx
@@ -1,12 +1,22 @@
 import { auth } from "@clerk/nextjs/server";
 import File from "./file";
 import { notFound } from "next/navigation";
+import { Card, CardTitle } from "@/components/ui/card";
+import { OrganizationList } from "@clerk/nextjs";
 
 const Page = () => {
     const {orgId} =auth()
     if(!orgId){
-        notFound()
+        <Card className="mx-auto justify-center flex flex-col gap-5 items-center m-5 p-5">
+        <CardTitle>
+            Create an organization to start.
+        </CardTitle>
+    <OrganizationList
+    hidePersonal/>
+
+    </Card>
     }
+    else
     return ( 
         <File orgId={orgId}/>
      );
