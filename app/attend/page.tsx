24a018e13a269c98824a3ec8ce1464e9c78bcac8
Tan Sven <iamsven2005@gmail.com>
2024-09-01 23:48:49 +0800

    bruh

--- app/attend/page.tsx
+++ app/attend/page.tsx
@@ -2,14 +2,14 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { attendance } from "@prisma/client";
 import dynamic from "next/dynamic";
-import { Redirect } from "@/components/redirect";
 import AttendList from "./AttendList";
+import { notFound } from "next/navigation";
 
 
 const Attend = async () => {
   const { userId } = auth();
   if (!userId) {
-    return <Redirect/>
+    return notFound()
   }
 
   const attendances: attendance[] = await db.attendance.findMany({
